e9c8db6a86fe9b6eec8ecdd1404667467206bef4
Axel Gromat <48716095+NegiAlba@users.noreply.github.com>
2021-08-20 09:45:30 +0200

    Update api.js

--- nextblog-front/lib/api.js
+++ nextblog-front/lib/api.js
@@ -10,20 +10,3 @@ export const fetchAPI = async (path) =>{
 
     return data;
 }
-
-
-==================================== medias.js ====================================
-    
-    export const getStrapiMedia = (url) => {
-    if (url == null) {
-        return null;
-    }
-
-    // If URL is on cloudinary
-    if(url.startWith("http")){
-        return url;
-    }
-
-    // If it's not, just return the file prepended by the server url
-    return `${process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337"}${url}`;
-}
